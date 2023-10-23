cf587d13a19e7f8ed412188c9c5427df91be6843
Charles <charlesthiagosantos@gmail.com>
2023-10-23 13:50:31 -0300

    c5 - conclusão do exercicio proposto anteriomente

---
+++ src/testeVector3/Main.java
@@ -0,0 +1,40 @@
+package testeVector3;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numero = sc.nextInt();
+		double soma , media;
+		
+		SomaVect[] vect = new SomaVect[numero];//definindo o final do vect
+		
+		double n = 0;
+		//populando vect
+		for(int i = 0; i < vect.length; i++) {
+			System.out.print("Digite o numero: ");
+			n = sc.nextDouble();
+			vect[i] = new SomaVect(n); //orientando que o vect[X] recebe um novo vect com o dados N
+		}
+		
+		//soma e media
+		soma = 0; 
+		for(int i =0; i < vect.length; i ++) {
+			soma += vect[i].getNumero();
+		}
+		
+		media = 0;
+		media += soma / numero;
+		
+		System.out.print("VALORES = ");
+		for(int i = 0; i < vect.length; i ++) {
+			System.out.printf("%.1f ", vect[i].getNumero());
+		}
+		
+		System.out.printf("\nSOMA = %.2f\n",soma);
+		System.out.printf("MEDIA = %.2f\n",media);
+		
+		sc.close();
+	}
+}
--- src/testeVector3/SomaVect.java
+++ src/testeVector3/SomaVect.java
@@ -1,5 +1,21 @@
 package testeVector3;
 
 public class SomaVect {
-
+	//atributos numero
+	private double numero;
+		
+	//construtor
+	public SomaVect (double numero) {
+		this.numero = numero;
+	}
+	
+	//get
+	public double getNumero() {
+		return this.numero;
+	}
+	
+	//metudo media
+	
+	//metdo saida (valores ,soma, media)
+	
 }
