f968ea1d2661598d92c3fcd17d4c32cec5c33f91
Charles <charlesthiagosantos@gmail.com>
2023-10-24 12:44:37 -0300

    c8 - atualização do exercicio de altura

--- src/testeVector4/Altura.java
+++ src/testeVector4/Altura.java
@@ -1,5 +1,39 @@
 package testeVector4;
 
 public class Altura {
-
+	//Fazer um programa para ler nome, idade e altura de N pessoas, conforme exemplo. Depois, mostrar na
+	//tela a altura média das pessoas, e mostrar também a porcentagem de pessoas com menos de 16 anos,
+	//bem como os nomes dessas pessoas caso houver. 
+	
+	//atributos + contador static
+	private static int contador = 1;
+	private String nome;
+	private int idade;
+	private double altura;
+	
+	//construtor
+	public Altura (String nome, int idade , double altura) {
+		Altura.contador++;
+		this.nome = nome;
+		this.idade = idade;
+		this.altura = altura;
+	}
+	
+	//get
+	public static int getContador() {
+		return Altura.contador;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+	
+	public double getAltura() {
+		return this.altura;
+	}
+	
 }
--- src/testeVector4/Main.java
+++ src/testeVector4/Main.java
@@ -1,5 +1,51 @@
 package testeVector4;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		//chamar o scanner
+		Scanner sc = new Scanner(System.in);
+		//pegar totalDeVect
+		System.out.print("Quantas pessoas serao digitadas? ");
+		int totalDeVect = sc.nextInt();
+		//criação do vect usando a quantidade de totalVect
+		Altura[] vect = new Altura[totalDeVect];
+		//loop for criação dos dados e variaveis fora
+		String nome; int idade; double altura;
+		for(int i = 0; i < vect.length; i ++) {
+			System.out.printf("Dados da %da pessoa:\n",Altura.getContador());
+			System.out.println("Nome:");
+			nome = sc.next();
+			System.out.println("Idade:");
+			idade = sc.nextInt();
+			System.out.println("Altura:");
+			altura = sc.nextDouble();
+			//final do loop atribuir os valor digitados
+			vect[i] = new Altura(nome, idade, altura);
+		}
+		//loop for para calcular a media da altura
+		double totalAltura = 0;double mediaDeMenor16 = 0;char modulo = '%';
+		for(int i = 0;i < vect.length; i ++) {
+			totalAltura += vect[i].getAltura();
+			if(vect[i].getIdade() < 16) {
+				mediaDeMenor16 ++;
+				}
+		}
+		System.out.printf("Altura média: %.2f\n", totalAltura / totalDeVect);
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%c%n", (mediaDeMenor16 * 100) / totalDeVect, modulo);
+	
+		//calculo para porcentagem
+		
+		// 16 anos pegar nome
+		String nomeMenor16;
+		for(int i = 0;i < vect.length; i ++) {
+			if(vect[i].getIdade() < 16) {
+			nomeMenor16 = vect[i].getNome();
+			System.out.printf("%s\n", vect[i].getNome());
+			}
+		}
+		//escaner fechado
+		sc.close();
+	}
 }
