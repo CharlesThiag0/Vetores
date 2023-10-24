c012203f18da61a186c53e1208916ee51d84b2c2
Charles <charlesthiagosantos@gmail.com>
2023-10-24 16:04:34 -0300

    c14 - conclusão do exercicio

--- src/testeVector7/Main.java
+++ src/testeVector7/Main.java
@@ -1,5 +1,36 @@
 package testeVector7;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quantos valores vai ter cada vetor?");
+		int n = sc.nextInt();
+		
+		SomaVect[] vectA = new SomaVect[n];
+		SomaVect[] vectB = new SomaVect[n];
+		SomaVect[] vectC = new SomaVect[n];
+		
+		int numeroA, numeroB, soma;
+		System.out.println("Digite os valores do vetor A:");
+		for(int i = 0; i < vectA.length; i++) {
+			numeroA = sc.nextInt();
+			vectA[i] = new SomaVect(numeroA);
+		}
+		
+		System.out.println("Digite os valores do vetor B:");
+		for(int i = 0; i < vectB.length; i++) {
+			numeroB = sc.nextInt();
+			vectB[i] = new SomaVect(numeroB);
+		}
+	
+		System.out.println("VETOR RESULTANTE:");
+		for(int i = 0; i < vectC.length; i ++) {
+			soma = vectA[i].getNumero() + vectB[i].getNumero();
+			System.out.println(soma);
+		}
+		
+		sc.close();
+	}
 }
--- src/testeVector7/SomaVect.java
+++ src/testeVector7/SomaVect.java
@@ -1,5 +1,15 @@
 package testeVector7;
 
 public class SomaVect {
-
+	private int numero;
+	
+	public SomaVect (int numero) {
+		this.numero = numero;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	
 }
