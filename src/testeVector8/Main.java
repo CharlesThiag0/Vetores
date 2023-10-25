4208b17734875d1a2c91a0150f001cc5502e36fc
Charles <charlesthiagosantos@gmail.com>
2023-10-25 14:01:54 -0300

    c18 - conclução do exercicio abaixo da media

--- src/testeVector8/Main.java
+++ src/testeVector8/Main.java
@@ -1,5 +1,35 @@
 package testeVector8;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quantos elementos vai ter o vetor?");
+		int n = sc.nextInt();
+		
+		double vect[] = new double[n];
+		
+		int qtd = 0;
+		for(int i = 0; i < vect.length; i++) {
+			System.out.println("Digite");
+			vect[i] = sc.nextDouble();
+			qtd ++;
+		}
+		
+		double media = 0;
+		for(int i = 0; i < vect.length; i++) {
+			media += vect[i];
+		}
+		double mediaFinal = media / qtd;
+		System.out.printf("Media de: %.3f%n",mediaFinal);	
+		
+		System.out.println("Abaixo da media:");
+		for(int i = 0; i < vect.length; i++) {
+			if(vect[i] < mediaFinal) {
+			System.out.println(vect[i]);
+			}
+		}
+		sc.close();
+	}
 }
