8954bbcadc56b7738dd4b184b9da9055643baff5
Charles <charlesthiagosantos@gmail.com>
2023-10-26 13:31:39 -0300

    c31 - melhorias do exercicio de altura e genero

--- src/testeVector12/AlturaG.java
+++ src/testeVector12/AlturaG.java
@@ -6,50 +6,53 @@ import testeImport2.AlturaGenero;
 public class AlturaG {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		int qtdF, qtdM;
+		double altura, soma, maiorAltura, menorAltura;
+		char genero;
+
 		System.out.print("Quantas pessoas serao digitadas? ");
 		int n = sc.nextInt();
-		
+
 		AlturaGenero vect[] = new AlturaGenero[n];
-		
-		double altura;
-		char genero;
-		
-		for(int i = 0; i < vect.length; i++) {
-			System.out.printf("Altura da %da pessoa: ", i +1);
+
+		for (int i = 0; i < vect.length; i++) {
+			System.out.printf("Altura da %da pessoa: ", i + 1);
 			altura = sc.nextDouble();
-			System.out.printf("Genero da %da pessoa: ", i +1);
+			System.out.printf("Genero da %da pessoa: ", i + 1);
 			genero = sc.next().charAt(0);
 			vect[i] = new AlturaGenero(altura, genero);
 		}
-		
-		double maiorAltura = Integer.MIN_VALUE;
-		double menorAltura = Integer.MAX_VALUE;
-		for(int i = 0; i < vect.length; i++) {
-			if(vect[i].getAltura() > maiorAltura) {
+
+		menorAltura = Integer.MAX_VALUE;
+		maiorAltura = Integer.MIN_VALUE;
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i].getAltura() > maiorAltura) {
 				maiorAltura = vect[i].getAltura();
 			}
-			
-			if(vect[i].getAltura() < menorAltura) {
+
+			if (vect[i].getAltura() < menorAltura) {
 				menorAltura = vect[i].getAltura();
+			}
 		}
-	}
-		System.out.printf("Menor altura: %.2f\n",menorAltura);
-		System.out.printf("Maior altura: %.2f\n",maiorAltura);
-		
-		double soma = 0;
-		int qtdF = 0;
-		int qtdM = 0;
-		for(int i = 0; i < vect.length; i++) {
-			if(vect[i].getGenero() == 'F') {
-			soma += vect[i].getAltura();
-			qtdF++;
-		} else {
-			qtdM++;
+
+		soma = 0;
+		qtdF = 0;
+		qtdM = 0;
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i].getGenero() == 'F') {
+				soma += vect[i].getAltura();
+				qtdF++;
+			} else {
+				qtdM++;
+			}
 		}
-		}
+
+		System.out.printf("Menor altura: %.2f\n", menorAltura);
+		System.out.printf("Maior altura: %.2f\n", maiorAltura);
 		System.out.printf("Media das alturas das mulheres = %.2f\n", soma / qtdF);
 		System.out.println("Numero de homens = " + qtdM);
-	
+
 		sc.close();
 	}
 }
