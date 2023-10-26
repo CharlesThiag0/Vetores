f8a65aea336b02103e6b0ee720b52530528a1010
Charles <charlesthiagosantos@gmail.com>
2023-10-26 12:42:00 -0300

    c27 - adição de um classe com altura e genero

---
+++ src/testeImport2/AlturaGenero.java
@@ -0,0 +1,19 @@
+package testeImport2;
+
+public class AlturaGenero {
+	private double altura;
+	private char genero;
+	
+	public AlturaGenero (double altura, char genero) {
+		this.altura = altura;
+		this.genero = genero;
+	}
+	
+	public double getAltura() {
+		return this.altura;
+	}
+	
+	public char getGenero() {
+		return this.genero;
+	}
+}
